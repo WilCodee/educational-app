14206a2f7da7ef34c7bc44064efde22f56def47c
Wil-Quispe <newwilcode@gmail.com>
2022-08-11 12:04:49 -0500

    create new activity

--- src/components/Forms/ActivityForm.tsx
+++ src/components/Forms/ActivityForm.tsx
@@ -1,159 +1,168 @@
-import React, { useState } from "react";
-import { Form, Input, Button, message, DatePicker, Select } from "antd";
-import { postData } from "src/services/fetch/postData";
-import { putData } from "src/services/fetch/putData";
-import moment from 'moment';
-import { ObjectId } from "bson";
-
+import React, { Dispatch, SetStateAction, useState } from 'react'
+import { Form, Input, Button, message, DatePicker, Select } from 'antd'
+import { postData } from 'src/services/fetch/postData'
+// import { putData } from "src/services/fetch/putData";
+import moment from 'moment'
+import type { FormInstance } from 'antd/es/form'
+// import { ObjectId } from "bson";
 
 interface IProps {
-    mode: string
-    data: any
-    courses: any[]
+  setReFetch: Dispatch<SetStateAction<boolean>>
+  mode: string
+  data: any
+  courses: any[]
 }
 
-const { Option } = Select;
+const { Option } = Select
 
-const ActivityForm = ({ mode, courses, data }: IProps) => {
-    const [isSubmitting, setIsSubmitting] = useState(false);
+const ActivityForm = ({ mode, courses, data, setReFetch }: IProps) => {
+  const formAddRef = React.createRef<FormInstance>()
+  const [isSubmitting] = useState(false)
 
-    const onFinishAdd = async (values: any) => {
-        console.log('values', values)
-    };
+  const onFinishAdd = async (values: any) => {
+    console.log({ values })
+    let request = await postData(`activities`, values)
+    if (request.status) {
+      formAddRef.current!.resetFields()
+      message.info(request.message)
+      setReFetch(true)
+    }
+  }
 
-    const onFinishEdit = async (values: any) => {
-        console.log('values')
-    };
+  const onFinishEdit = async (values: any) => {
+    console.log('values')
+  }
 
-    const onFinishFailed = (errorInfo: any) => {
-        message.error('Algo ha salido mal, reinténtalo más tarde o ponte en contacto con los administradores del sistema');
-    };
+  const onFinishFailed = (errorInfo: any) => {
+    message.error(
+      'Algo ha salido mal, reinténtalo más tarde o ponte en contacto con los administradores del sistema'
+    )
+  }
 
+  return (
+    <>
+      {mode === 'ADD' && (
+        <Form
+          ref={formAddRef}
+          name='basic'
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 12 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinishAdd}
+          onFinishFailed={onFinishFailed}
+          autoComplete='off'
+        >
+          <Form.Item
+            label='Título'
+            name='title'
+            rules={[
+              { required: true, message: 'Ingresa el título de la actividad' },
+            ]}
+          >
+            <Input placeholder='Tarea #1' />
+          </Form.Item>
 
-    return (
-        <>
-            {mode === "ADD" && (
-                <Form
-                    name="basic"
-                    labelCol={{ span: 8 }}
-                    wrapperCol={{ span: 12 }}
-                    initialValues={{ remember: true }}
-                    onFinish={onFinishAdd}
-                    onFinishFailed={onFinishFailed}
-                    autoComplete="off"
-                >
-                    <Form.Item
-                        label="Título"
-                        name="title"
-                        rules={[{ required: true, message: "Ingresa el título de la actividad" }]}
-                    >
-                        <Input placeholder="Tarea #1" />
-                    </Form.Item>
+          <Form.Item
+            label='Descripción'
+            name='description'
+            rules={[
+              { required: true, message: 'Ingresa el título de la actividad' },
+            ]}
+          >
+            <Input placeholder='Completar las páginas 5 y 6' />
+          </Form.Item>
 
+          <Form.Item
+            label='Fecha de cierre'
+            name='closeDate'
+            rules={[{ required: true, message: 'Ingresa la fecha de cierre' }]}
+          >
+            <DatePicker />
+          </Form.Item>
 
-                    <Form.Item
-                        label="Descripción"
-                        name="description"
-                        rules={[{ required: true, message: "Ingresa el título de la actividad" }]}
-                    >
-                        <Input placeholder="Completar las páginas 5 y 6" />
-                    </Form.Item>
+          <Form.Item
+            label='Curso'
+            name='course_id'
+            rules={[{ required: true, message: 'Seleccione un curso!!' }]}
+          >
+            <Select placeholder='Seleccionar curso'>
+              {courses.map((course: any) => (
+                <Option value={course._id}>{course.name}</Option>
+              ))}
+            </Select>
+          </Form.Item>
 
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type='primary' htmlType='submit' loading={isSubmitting}>
+              Agregar Actividad
+            </Button>
+          </Form.Item>
+        </Form>
+      )}
+      {mode === 'EDIT' && (
+        <Form
+          initialValues={{
+            title: data.title,
+            description: data.description,
+            closeDate: moment(data.closeDate),
+            course_id: data.course_id,
+          }}
+          name='basic'
+          labelCol={{ span: 10 }}
+          wrapperCol={{ span: 16 }}
+          onFinish={onFinishEdit}
+          onFinishFailed={onFinishFailed}
+          autoComplete='off'
+        >
+          <Form.Item
+            label='Título'
+            name='title'
+            rules={[
+              { required: true, message: 'Ingresa el título de la actividad' },
+            ]}
+          >
+            <Input placeholder='Tarea #1' />
+          </Form.Item>
 
-                    <Form.Item
-                        label="Fecha de cierre"
-                        name="closeDate"
-                        rules={[{ required: true, message: "Ingresa la fecha de cierre" }]}
-                    >
-                        <DatePicker />
-                    </Form.Item>
+          <Form.Item
+            label='Descripción'
+            name='description'
+            rules={[
+              { required: true, message: 'Ingresa el título de la actividad' },
+            ]}
+          >
+            <Input placeholder='Completar las páginas 5 y 6' />
+          </Form.Item>
 
+          <Form.Item
+            label='Fecha de cierre'
+            name='closeDate'
+            rules={[{ required: true, message: 'Ingresa la fecha de cierre' }]}
+          >
+            <DatePicker />
+          </Form.Item>
 
-                    <Form.Item
-                        label="Curso"
-                        name="course_id"
-                        rules={[{ required: true, message: "Seleccione un curso!!" }]}
-                    >
-                        <Select placeholder="Seleccionar curso" >
-                            {
-                                courses.map((course: any) => <Option value={course._id}>{course.name}</Option>)
-                            }
-                        </Select>
-                    </Form.Item>
+          <Form.Item
+            label='Curso'
+            name='course_id'
+            rules={[{ required: true, message: 'Seleccione un curso!!' }]}
+          >
+            <Select placeholder='Seleccionar curso'>
+              {courses.map((course: any) => (
+                <Option value={course._id}>{course.name}</Option>
+              ))}
+            </Select>
+          </Form.Item>
 
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type='primary' htmlType='submit' loading={isSubmitting}>
+              Editar Actividad
+            </Button>
+          </Form.Item>
+        </Form>
+      )}
+    </>
+  )
+}
 
-                   
-                    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-
-                        <Button type="primary" htmlType="submit" loading={isSubmitting}>
-                            Agregar Actividad
-                        </Button>
-                    </Form.Item>
-                </Form>
-            )}
-            {mode === "EDIT" && (
-                <Form
-                    initialValues={{
-                        title: data.title,
-                        description: data.description,
-                        closeDate: moment(data.closeDate),
-                        course_id: data.course_id
-                    }}
-                    name="basic"
-                    labelCol={{ span: 10 }}
-                    wrapperCol={{ span: 16 }}
-                    onFinish={onFinishEdit}
-                    onFinishFailed={onFinishFailed}
-                    autoComplete="off"
-                >
-                      <Form.Item
-                        label="Título"
-                        name="title"
-                        rules={[{ required: true, message: "Ingresa el título de la actividad" }]}
-                    >
-                        <Input placeholder="Tarea #1" />
-                    </Form.Item>
-
-
-                    <Form.Item
-                        label="Descripción"
-                        name="description"
-                        rules={[{ required: true, message: "Ingresa el título de la actividad" }]}
-                    >
-                        <Input placeholder="Completar las páginas 5 y 6" />
-                    </Form.Item>
-
-
-                    <Form.Item
-                        label="Fecha de cierre"
-                        name="closeDate"
-                        rules={[{ required: true, message: "Ingresa la fecha de cierre" }]}
-                    >
-                        <DatePicker />
-                    </Form.Item>
-
-
-                    <Form.Item
-                        label="Curso"
-                        name="course_id"
-                        rules={[{ required: true, message: "Seleccione un curso!!" }]}
-                    >
-                        <Select placeholder="Seleccionar curso" >
-                            {
-                                courses.map((course: any) => <Option value={course._id}>{course.name}</Option>)
-                            }
-                        </Select>
-                    </Form.Item>
-
-                    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                        <Button type="primary" htmlType="submit" loading={isSubmitting}>
-                            Editar Actividad
-                        </Button>
-                    </Form.Item>
-                </Form>
-            )}
-        </>
-    );
-};
-
-export default ActivityForm; 
+export default ActivityForm
--- src/pages/Teachers/ActivitiesPage.tsx
+++ src/pages/Teachers/ActivitiesPage.tsx
@@ -1,79 +1,157 @@
-import { Button, Card, Modal, Select, Skeleton } from 'antd';
-import React, { useState, useEffect, useContext } from 'react';
-import ActivityForm from 'src/components/Forms/ActivityForm';
-import { AuthContext } from 'src/context/AuthContext';
-import { getData } from 'src/services/fetch/getData';
-
-
+import { Button, Card, Modal, Select, Skeleton, Table } from 'antd'
+import React, { useState, useEffect, useContext } from 'react'
+import ActivityForm from 'src/components/Forms/ActivityForm'
+import { AuthContext } from 'src/context/AuthContext'
+import { getData } from 'src/services/fetch/getData'
+import moment from 'moment'
+import { DeleteOutlined, EditOutlined } from '@ant-design/icons'
 const { Option } = Select
 
 const ActivitiesPage = () => {
-    const { user } = useContext(AuthContext);
-    const [courses, setCourses] = useState([])
-    const [loading, setLoading] = useState(false)
-    const [showModal, setShowModal] = useState(false)
-    const [mode, setMode] = useState("ADD")
+  const { user } = useContext(AuthContext)
+  const [courses, setCourses] = useState([])
+  const [activities, setActivities] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [showModal, setShowModal] = useState(false)
+  const [mode, setMode] = useState('ADD')
+  const [filteredById, setFilteredById] = useState<string>()
+  const [reFetchActivities, setReFetchActivities] = useState(false)
 
-    const initialRequest = async () => {
-        setLoading(true);
-        let request = await getData(`courses_by_teacher/${user._id}`);
-        if (request.status) {
-            const coursesToTable = request.courses.map((course: any) => {
-                course.key = course["_id"];
-                return course;
-            });
-            setCourses(coursesToTable);
-            setLoading(false);
-        }
-    };
+  const columns = [
+    {
+      title: 'Titulo',
+      dataIndex: 'title',
+      key: 'title',
+    },
+    {
+      title: 'Descripción',
+      dataIndex: 'description',
+      key: 'description',
+    },
+    {
+      title: 'Fecha de Cierre',
+      dataIndex: 'closeDate',
+      key: 'closeDate',
+      render: (item: any) => (
+        <span>{`${moment(item).format('MMMM Do YYYY, h:mm:ss a')}`}</span>
+      ),
+    },
+    {
+      title: 'Acción',
+      key: 'action',
+      render: () => {
+        return (
+          <>
+            <EditOutlined onClick={handleUpdateActivity} />
+            {'  '}
+            <DeleteOutlined />
+          </>
+        )
+      },
+    },
+  ]
 
-    useEffect(() => {
-        initialRequest();
-    }, []);
-
-    const handleAddActivity = () => {
-        setShowModal(true)
-        setMode("ADD")
+  const initialRequest = async () => {
+    setLoading(true)
+    let request = await getData(`courses_by_teacher/${user._id}`)
+    if (request.status) {
+      const coursesToTable = request.courses.map((course: any) => {
+        course.key = course['_id']
+        return course
+      })
+      setCourses(coursesToTable)
+      setLoading(false)
     }
+  }
 
-    const handleUpdateActivity = () => {
-        setShowModal(true)
-        setMode("UPDATE")
+  const getActivities = async () => {
+    const request = await getData(`/course/${filteredById}/activities`)
+    if (request.status) {
+      const activitiesToTable = request.activities.map((course: any) => {
+        course.key = course['_id']
+        return course
+      })
+      setActivities(activitiesToTable)
     }
+  }
 
-    const handleCancel = () => {
-        setShowModal(false)
-        setMode("ADD")
-    }
+  useEffect(() => {
+    initialRequest()
+  }, [])
 
-    return (
-        <Card title="Actividades" extra={<Button type="primary" onClick={handleAddActivity}  >Crear actividad</Button>} >
+  useEffect(() => {
+    getActivities()
+    setReFetchActivities(false)
+  }, [filteredById, reFetchActivities])
 
-            {
-                loading ? <Skeleton /> :
-                    <div>
-                        <span>Filtrar por:
-                            <Select placeholder="Seleccionar curso" >
-                                {
-                                    courses.map((course: any) => <Option value={course._id}>{course.name}</Option>)
-                                }
-                            </Select>
-                        </span>
+  const handleAddActivity = () => {
+    setShowModal(true)
+    setMode('ADD')
+  }
 
-                        {
-                            showModal &&
-                            <Modal title={mode === "ADD" ? "Nueva Actividad" : "Actualizar Actividad"} visible={showModal} footer={null} onCancel={handleCancel} >
-                                <ActivityForm
-                                    mode={mode}
-                                    courses={courses}
-                                    data={mode === "ADD" ? {} : {}}
-                                />
-                            </Modal>
-                        }
-                    </div>
-            }
-        </Card>
-    )
+  const handleUpdateActivity = () => {
+    setShowModal(true)
+    setMode('UPDATE')
+  }
+
+  const handleCancel = () => {
+    setShowModal(false)
+    setMode('ADD')
+  }
+
+  return (
+    <Card
+      title='Actividades'
+      extra={
+        <Button type='primary' onClick={handleAddActivity}>
+          Crear actividad
+        </Button>
+      }
+    >
+      {loading ? (
+        <Skeleton />
+      ) : (
+        <div>
+          <span>
+            Filtrar por:
+            <Select
+              placeholder='Seleccionar curso'
+              onSelect={(value: string) => {
+                setFilteredById(value)
+              }}
+            >
+              {courses.map((course: any) => (
+                <Option value={course._id}>{course.name}</Option>
+              ))}
+            </Select>
+          </span>
+          <br />
+          <br />
+          <div>
+            <Table columns={columns} dataSource={activities} />
+          </div>
+
+          {showModal && (
+            <Modal
+              title={
+                mode === 'ADD' ? 'Nueva Actividad' : 'Actualizar Actividad'
+              }
+              visible={showModal}
+              footer={null}
+              onCancel={handleCancel}
+            >
+              <ActivityForm
+                setReFetch={setReFetchActivities}
+                mode={mode}
+                courses={courses}
+                data={mode === 'ADD' ? {} : {}}
+              />
+            </Modal>
+          )}
+        </div>
+      )}
+    </Card>
+  )
 }
 
-export default ActivitiesPage; 
+export default ActivitiesPage
